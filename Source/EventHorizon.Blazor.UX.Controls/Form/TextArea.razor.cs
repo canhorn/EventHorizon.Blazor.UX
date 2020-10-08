82548a9462885b38235182f532709ef86004d9ee
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-10-07 23:49:35 -0500

    New Text Area Control Create new Text Area form control. Fixed scrollbar and resizer styles.

---
+++ Source/EventHorizon.Blazor.UX.Controls/Form/TextArea.razor.cs
@@ -0,0 +1,43 @@
+﻿namespace EventHorizon.Blazor.UX.Controls.Form
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Components;
+
+    public class TextAreaModel
+        : ComponentBase
+    {
+        [Parameter]
+        public string id { get; set; }
+        [Parameter]
+        public string name { get; set; }
+        [Parameter]
+        public string @class { get; set; }
+        [Parameter]
+        public string LabelDisplay { get; set; }
+        [Parameter]
+        public bool disabled { get; set; }
+        private string _value;
+        [Parameter]
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value) return;
+                _value = value;
+                ValueChanged.InvokeAsync(
+                    value
+                );
+            }
+        }
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+        [Parameter]
+        public int rows { get; set; } = 3;
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IDictionary<string, object> Attributes { get; set; }
+
+        public string Uid { get; private set; } = Guid.NewGuid().ToString();
+    }
+}
