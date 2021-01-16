550f18acbf9eb084f794dcff93da6ac05ecc2da2
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-01-16 09:06:26 -0600

    Removed Sound from Button

--- Source/EventHorizon.Blazor.UX/Button/Button.razor.cs
+++ Source/EventHorizon.Blazor.UX/Button/Button.razor.cs
@@ -4,20 +4,14 @@
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
     using Microsoft.AspNetCore.Components.Web;
-    using Microsoft.JSInterop;
 
     public class ButtonModel
         : ComponentBase
     {
-        [Inject]
-        public IJSRuntime JSRuntime { get; set; }
-
         [Parameter]
         public EventCallback<MouseEventArgs> OnClick { get; set; }
         [Parameter]
         public RenderFragment ChildContent { get; set; }
-        [Parameter]
-        public string SoundPath { get; set; } = "/_content/EventHorizon.Blazor.UX/sounds/click.mp3";
         [Parameter(CaptureUnmatchedValues = true)]
         public IDictionary<string, object> Attributes { get; set; }
 
@@ -30,21 +24,9 @@
             await OnClick.InvokeAsync(args);
 
             Animation = "--blink";
-            if (!string.IsNullOrEmpty(SoundPath))
-            {
-                // TODO: Create Sound Service
-                await JSRuntime.InvokeVoidAsync(
-                    "ehzBlazorUx.playAudioFile",
-                    SoundPath
-                );
-            }
             // TODO: Create Delay Service
-            await Task.Run(() =>
-            {
-                System.Threading.Thread.Sleep(10);
-                Animation = string.Empty;
-                InvokeAsync(StateHasChanged);
-            });
+            await Task.Delay(10);
+            Animation = string.Empty;
         }
     }
 }
